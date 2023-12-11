ef3255b24716ec662ec0a4fde16b0e2cfcd66940
thanos101201 <ayushsingh10122001@gmail.com>
2023-12-12 00:05:27 +0530

    added textual data

--- src/Components/AboutComponent.js
+++ src/Components/AboutComponent.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import NavComponent from './NavComponent'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { CardHeader, Card, CardImg, CardTitle, CardBody } from 'reactstrap'
+import { CardHeader, Card, CardImg, CardTitle, CardBody, CardText } from 'reactstrap'
 import Sarvaha from '../assets/sarvaha.jpg';
 import Dalhousie from '../assets/Dalhousie.png';
 import SIH from '../assets/sih.png';
@@ -108,32 +108,31 @@ function About() {
           </div>
           <div className='col-10 col-md-8 d-flex align-items-center'>
             <p style={{color:'white'}}>
-              bvdfkjvnjdlf
-              dfkhbvsdkjflvnksd
-              dbjvbdfkvjdfsv
-              djvbdfjkjsdfv
-              ndfjvbdfjkad
-              djfvbhsdkjsdv
-              dfkjvnlsdfv
-              dfvbkshdjfvnjldsfv'
-              dfjbvdhfvkhsdjvsdf
-              dfnvkljdsfjvn
+              <h4>Sarvaha Systems</h4>
+              <h5>Software Development Engineer</h5>
+              <h6><i>July 2023</i> to <i>Present</i></h6>
+              Responsibilities :
+              <ul>
+                <li>Design structure of plugin which will be integrated to the monitoring system</li>
+                <li>Implement the designed plugin and integrate the plugin to the monitoing system</li>
+                <li>Maintain the fitness game application</li>
+                <li>Tech stack : ASP.NET, React Js, Node JS, Express JS, Mongo DB, MySQL, Docker, Kubernetes, Convolutional Neural Network</li>
+              </ul>
             </p>
           </div>
         </div>
         <div className='row d-flex justify-content-center m-3'>
           <div className='col-10 col-md-8 m-3 d-flex align-items-center'>
             <p style={{color:'white'}}>
-              bvdfkjvnjdlf
-              dfkhbvsdkjflvnksd
-              dbjvbdfkvjdfsv
-              djvbdfjkjsdfv
-              ndfjvbdfjkad
-              djfvbhsdkjsdv
-              dfkjvnlsdfv
-              dfvbkshdjfvnjldsfv'
-              dfjbvdhfvkhsdjvsdf
-              dfnvkljdsfjvn
+            <h4>Aeons Technologies</h4>
+              <h5>Co-Founder</h5>
+              <h6><i>Augst 2023</i> to <i>Present</i></h6>
+              Responsibilities :
+              <ul>
+                <li>Manage the client products as well as design the solutions based on the client requirements</li>
+                <li>Manage the development of the solution based on the proposed design based on the client requirement</li>
+                <li>Tech stack and paradigms : DevOps, Git, React Js, Node JS, Express JS, Mongo DB, MySQL, Docker, Kubernetes, Convolutional Neural Network</li>
+              </ul>
             </p>
           </div>
           <div className='col-10 col-md-3 m-3 d-flex align-items-center'>
@@ -154,38 +153,36 @@ function About() {
           </div>
           <div className='col-10 col-md-8 m-3 d-flex align-items-center'>
             <p style={{color:'white'}}>
-              bvdfkjvnjdlf
-              dfkhbvsdkjflvnksd
-              dbjvbdfkvjdfsv
-              djvbdfjkjsdfv
-              ndfjvbdfjkad
-              djfvbhsdkjsdv
-              dfkjvnlsdfv
-              dfvbkshdjfvnjldsfv'
-              dfjbvdhfvkhsdjvsdf
-              dfnvkljdsfjvn
+            <h4>Sarvaha Systems</h4>
+              <h5>Software Development Intern</h5>
+              <h6><i>January 2023</i> to <i>July 2023</i></h6>
+              Responsibilities :
+              <ul>
+                <li>Develop accelerator modules for tracking the usage of different IT services and assets done by any organization</li>
+                <li>Test and maintain the accelerator modules based on different real life scenarios</li>
+                <li>Tech stack and paradigms : DevOps, Git, Python, Tensorflow Docker, Kubernetes, Convolutional Neural Network</li>
+              </ul>
             </p>
           </div>
         </div>
         <div className='row d-flex justify-content-center m-3'>
           <div className='col-10 col-md-8 m-3 d-flex align-items-center'>
             <p style={{color:'white'}}>
-              bvdfkjvnjdlf
-              dfkhbvsdkjflvnksd
-              dbjvbdfkvjdfsv
-              djvbdfjkjsdfv
-              ndfjvbdfjkad
-              djfvbhsdkjsdv
-              dfkjvnlsdfv
-              dfvbkshdjfvnjldsfv'
-              dfjbvdhfvkhsdjvsdf
-              dfnvkljdsfjvn
+            <h4>Dalhousie University, Halifax Canada</h4>
+              <h5>Research Intern</h5>
+              <h6><i>June 2022</i> to <i>September 2022</i></h6>
+              Responsibilities :
+              <ul>
+                <li>Develop mobile game to promote physical activities among different individuals of different age group</li>
+                <li>Manage the data gathered by the game, and prepare it for analysis</li>
+                <li>Tech stack : React Native, Express Js, Node JS, Google Apis, Mongo Db</li>
+              </ul>
             </p>
           </div>
           <div className='col-10 col-md-3 m-3 d-flex align-items-center'>
             <Card style={{height:'4rem', width:'4rem', backgroundColor:'black'}}>
               <CardHeader>
-                <CardImg style={{height:'4rem', width:'4rem'}} src={Dalhousie} />
+                <CardImg style={{height:'6rem', width:'6rem'}} src={Dalhousie} />
               </CardHeader>
             </Card>
           </div>
@@ -202,22 +199,19 @@ function About() {
           <div className='col-10 col-md-3 m-3 d-flex align-items-center'>
             <Card style={{height:'4rem', width:'4rem', backgroundColor:'black'}}>
               <CardHeader>
-                <CardImg style={{height:'7rem', width:'7rem'}} src={SIH} />
+                <CardImg style={{height:'4rem', width:'4rem'}} src={SIH} />
               </CardHeader>
             </Card>
           </div>
           <div className='col-10 col-md-8 m-3 d-flex align-items-center'>
             <p style={{color:'white'}}>
-              bvdfkjvnjdlf
-              dfkhbvsdkjflvnksd
-              dbjvbdfkvjdfsv
-              djvbdfjkjsdfv
-              ndfjvbdfjkad
-              djfvbhsdkjsdv
-              dfkjvnlsdfv
-              dfvbkshdjfvnjldsfv'
-              dfjbvdhfvkhsdjvsdf
-              dfnvkljdsfjvn
+            <h5>Smart India Hackathon 2022</h5>
+              Responsibilities :
+              <ul>
+                <li>Develop mobile game to promote physical activities among different individuals of different age group</li>
+                <li>Manage the data gathered by the game, and prepare it for analysis</li>
+                <li>Tech stack : React Native, Express Js, Node JS, Google Apis, Mongo Db</li>
+              </ul>
             </p>
           </div>
         </div>
@@ -240,7 +234,7 @@ function About() {
           <div className='col-10 col-md-3 m-3 d-flex align-items-center'>
             <Card style={{height:'4rem', width:'4rem', backgroundColor:'black'}}>
               <CardHeader>
-                <CardImg style={{height:'7rem', width:'7rem'}} src={SIH} />
+                <CardImg style={{height:'4rem', width:'4rem'}} src={SIH} />
               </CardHeader>
             </Card>
           </div>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -12,21 +12,24 @@ function Home() {
         <div className='row d-flex justify-content-center'>
           <div className='col-12 col-md-6 d-flex align-items-center'>
             <p style={{color:'white'}}>
-              sdnjs
-              sdvnasldkvasdv
-              sndvalkvasvnjafsl
-              sdnvjasldv m,adsfv
-              sdlvjafsndvadfsv
-              sjavl df,v adsflvnfdv
-              kjdsvn asndflvn asndflvnsfsdk
-              skjdvnasdlfvnlsdfv
-              akdjfvansfjldvnaksvas
-              dfskjvnasdflaksvk;asf;vitals
-              sjklvnasljnvlksdklv;as;kvjlafsv
-              dkjflvnaslfjkvmasfmv;as;vitalskjdfv
-              kjdfvakjlsdfvljadsflkvnadsfkv
-              jsdflvansldvlkasvklasfklvakldfvkandf
-              kjasdnvlsdvlkasv;kjasdnvlsdvlkasvlsfvlnfsvladsf
+              I am Pratiksingh Rajeshsingh Thakur. I am working as a Software Development Engineer at Sarvaha Systems, Pune.
+              I have completed my Bachelor's degree in Computer Science and Engineering from Shri Guru Gobind Singhji Institute
+              of Engineering and Technology, Nanded. I am a Mitacs GRI scholar of batch 2022.
+              <br></br>
+              <br></br>
+              During my internship at Dalhousie University, I worked at a mobile application for promoting physical activities
+              among number of individuals of different age group. The application collected data related to different physical
+              activites performed by different individuals. The data contained a trend with itself. This application bring my
+              notice to the analysis such data can bring regarding the physical as well as mental health of individuals. Applications
+              similar to the application I mentioned, can be used to gather data related to different physical activites, and sensor data
+              related to emotions. Once these details can be gathered using the wearables and smart phones, the data can be used for training
+              deep learning models such that it can be used to make predictions regarding mental health and physical health stability.
+              <br></br>
+              <br></br>
+              For example the geographical co-ordinates of places visited by individuals can be used to learn how any individual visit different
+              places. Frequency of places being visited by different individuals can help us to learn the tendency of people to spend their time.
+              Similarly there can be more such activity data that can be used to analyze and make predictions regarding mental health and physical 
+              health of different individuals.
             </p>
           </div>
           <div className='col-12 col-md-6'>
